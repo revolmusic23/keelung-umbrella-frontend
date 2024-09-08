a560f23e9dcf76bd2b2cdf2a3a97fe8bf83d92e7
revolmusic23 <revolmusic23@gmail.com>
2024-09-08 17:10:39 +0800

    add build dir in vite and add base url in yml

--- vite.config.js
+++ vite.config.js
@@ -20,5 +20,9 @@ export default defineConfig(({ mode }) => {
         '@': path.resolve(__dirname, './src'),
       },
     },
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
+    },
   };
 });
