170199449f8dec197d107c6f382a9be7f1299e22
revolmusic23 <revolmusic23@gmail.com>
2024-09-08 15:22:43 +0800

    move BASE_URL param in defineConfig and npm i again

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,26 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 const isDevelopment = process.env.NODE_ENV === 'development';
-const BASE_URL = isDevelopment ? '/' : '/keelung-umbrella-frontend/';
-console.log('Current BASE_URL:', BASE_URL);
+// const BASE_URL = isDevelopment ? '/' : '/keelung-umbrella-frontend/';
+// console.log('Current BASE_URL:', BASE_URL);
 
-export default defineConfig({
-  base: '/keelung-umbrella-frontend/',
-  plugins: [vue()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "@/assets/styles/main.scss" as *;`,
+export default defineConfig(({ mode }) => {
+  const BASE_URL = mode === 'production' ? '/keelung-umbrella-frontend/' : '/';
+
+  return {
+    base: BASE_URL,
+    plugins: [vue()],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/assets/styles/main.scss" as *;`,
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+  };
 });
