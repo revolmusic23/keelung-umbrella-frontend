6f697febb817f2cb7593159926cb27abc0736882
revolmusic23 <revolmusic23@gmail.com>
2024-09-08 15:36:03 +0800

    add env files

--- vite.config.js
+++ vite.config.js
@@ -2,27 +2,21 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
-const isDevelopment = process.env.NODE_ENV === 'development';
-// const BASE_URL = isDevelopment ? '/' : '/keelung-umbrella-frontend/';
-// console.log('Current BASE_URL:', BASE_URL);
+const BASE_URL = process.env.VITE_BASE_URL || '/';
 
-export default defineConfig(({ mode }) => {
-  const BASE_URL = mode === 'production' ? '/keelung-umbrella-frontend/' : '/';
-
-  return {
-    base: BASE_URL,
-    plugins: [vue()],
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: `@use "@/assets/styles/main.scss" as *;`,
-        },
+export default defineConfig({
+  base: BASE_URL,
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/assets/styles/main.scss" as *;`,
       },
     },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-  };
+  },
 });
