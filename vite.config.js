064b2684a3f6ddc789f44ac0dad86cbaf02cc9c8
revolmusic23 <revolmusic23@gmail.com>
2024-09-08 12:26:25 +0800

    add base url in vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
+  base: '/keelung-umbrella-frontend/',
   plugins: [vue()],
   css: {
     preprocessorOptions: {
