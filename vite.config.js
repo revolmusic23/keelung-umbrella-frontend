b289733891b5a93a6e4360ad3ceed721f5879864
revolmusic23 <revolmusic23@gmail.com>
2024-09-08 17:17:08 +0800

    use loadEnv in vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig(({ mode }) => {
   const BASE_URL = mode === 'production' ? '/keelung-umbrella-frontend/' : '/';
+  const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    base: '/keelung-umbrella-frontend/',
+    base: env.VITE_BASE_URL || '/keelung-umbrella-frontend/',
     plugins: [vue()],
     css: {
       preprocessorOptions: {
