b2c886e789b342f54009e13a603f907103f7cd57
revolmusic23 <revolmusic23@gmail.com>
2024-09-08 18:08:56 +0800

    rm config.js, rm html-transform in vite, rm redirect in index

--- src/config.js
+++
@@ -1,3 +0,0 @@
-const isDevelopment = process.env.NODE_ENV === 'development';
-
-export const BASE_URL = isDevelopment ? '/' : '/keelung-umbrella-frontend/';
--- vite.config.js
+++ vite.config.js
@@ -3,29 +3,11 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig(({ mode }) => {
-  // const BASE_URL = mode === 'production' ? '/keelung-umbrella-frontend/' : '/';
   const env = loadEnv(mode, process.cwd(), '');
-  const base = env.VITE_BASE_URL || '/keelung-umbrella-frontend/';
 
   return {
-    base: env.VITE_BASE_URL || '/keelung-umbrella-frontend/',
-    plugins: [
-      vue(),
-      {
-        name: 'html-transform',
-        transformIndexHtml(html) {
-          return html
-            .replace(
-              /<script type="module" src="\/src\/main.js"><\/script>/,
-              `<script type="module" src="${base}src/main.js"></script>`
-            )
-            .replace(
-              /href="\/images\/sm-umbrella.png"/,
-              `href="${base}images/sm-umbrella.png"`
-            );
-        },
-      },
-    ],
+    base: env.VITE_BASE_URL,
+    plugins: [vue()],
     css: {
       preprocessorOptions: {
         scss: {
