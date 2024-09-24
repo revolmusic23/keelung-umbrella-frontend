1dc7d05739b1fc2cc5acf61132d69ca1798d1d2e
revolmusic23 <revolmusic23@gmail.com>
2024-09-24 22:13:01 +0800

    feat: 首頁和活動頁面切版

--- src/views/Activity.vue
+++ src/views/Activity.vue
@@ -1,12 +1,110 @@
 <template>
   <v-sheet
-    class="mx-auto pa-4 rounded-lg border"
+    class="mx-auto pa-md-8 pa-4 rounded-lg border"
     elevation="4"
     max-width="1200"
   >
-    <div v-for="(paragraph, idx) in paragraphs" :key="idx">
-      <PageTitle :title="paragraph.title" class="mb-4" />
-      <p v-html="paragraph.content" class="mb-4 ml-2"></p>
+    <h1 class="page-title">一起「傘人」1~2~3！</h1>
+    <h3 class="page-subtitle">傘人家族創意合照 - 手機攝影比賽</h3>
+    <div class="paragraph">
+      <h4 class="paragraph-title">活動內容</h4>
+      <p>
+        10/10～10/31
+        活動期間至基隆文化中心前廣場與任一傘人家族成員合照，合照對象不限(人、竉物、玩偶、玩具…皆可)，
+        再至活動網站，選擇一張合照並留下一句話，上傳作品，即完成參賽，
+        活動結束後將選出前三名最有創意的照片，贈予精美獎品。
+      </p>
+      <br />
+
+      <div class="d-md-flex justify-space-between gx-6">
+        <p>
+          <span class="font-weight-bold">獎項</span><br />
+          第 1 名：<span class="orange-text font-weight-bold"
+            >LEICA SOFORT 2 拍立得相機</span
+          ><br />
+          第 2 名：<span class="orange-text font-weight-bold"
+            >Polaroid Now+ 拍立得相機 </span
+          ><br />
+          第 3 名：<span class="orange-text font-weight-bold"
+            >FUJIFILM instax SQUARE SQ1</span
+          ><br />
+        </p>
+        <div
+          class="img-container d-flex w-md-50 w-sm-75 w-100"
+          style="gap: 1.2rem"
+        >
+          <v-img src="images/prize-1.png"></v-img>
+          <v-img src="images/prize-2.png"></v-img>
+          <v-img src="images/prize-3.png"></v-img>
+        </div>
+      </div>
+    </div>
+
+    <div class="paragraph">
+      <h4 class="paragraph-title">活動好禮</h4>
+      <p>
+        <b>
+          <span class="orange-text">"限於活動現場"</span>
+          完成參賽後，分享活動網站於個人IG或FB並追路基隆市文化觀光局臉書，經現場工作人員確認無
+          誤後，將可獲得活動特製<span class="orange-text"
+            >「傘人家族精緻小物」</span
+          >一個（二款随機赠送，一人限領取一個，每日限量，送完為止）。
+        </b>
+      </p>
+    </div>
+
+    <div class="paragraph">
+      <h4 class="paragraph-title">收件時間</h4>
+      <p>
+        2024 年
+        <span class="date-text">10/10</span> 至
+        <span class="date-text">10/31</span>
+        止，逾期者恕不受理。
+      </p>
+    </div>
+
+    <div class="paragraph">
+      <h4 class="paragraph-title">評分方式</h4>
+      <p>
+        邀請基隆市政府文化觀光局人員及網紅伊米塔擔任評審，選出前三名作品。<br />
+        <br />
+        評分項目：
+      </p>
+      <v-table class="border">
+        <thead>
+          <tr>
+            <th class="text-center">創意</th>
+            <th class="text-center">文案</th>
+            <th class="text-center">品質</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>60%</td>
+            <td>30%</td>
+            <td>20%</td>
+          </tr>
+        </tbody>
+      </v-table>
+    </div>
+
+    <div class="paragraph">
+      <h4 class="paragraph-title">收件方式</h4>
+      <p>
+        一律於活動網站，上傳作品：<br />
+        <a :href="WEBSITE_URL" target="_blank" class="link">
+          {{ WEBSITE_URL }}
+        </a>
+      </p>
+    </div>
+
+    <div class="paragraph">
+      <h4 class="paragraph-title">得獎公佈</h4>
+      <p>
+        <span class="date-text">11/7</span> 將於<span class="orange-text"
+          >活動網站、文化觀光局臉書、魅力基隆臉書</span
+        >公佈前三名得獎名單，並通知得獎者領獎。
+      </p>
     </div>
   </v-sheet>
 </template>
@@ -19,13 +117,4 @@ import { WEBSITE_URL } from '@/utils/constants';
 import PageTitle from '@/components/PageTitle.vue';
 </script>
 
-<style lang="scss">
-.paragraph-title {
-  margin-top: 1em;
-  margin-bottom: 1em;
-}
-ul,
-ol {
-  margin-left: 1.4em;
-}
-</style>
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,75 @@
 <template>
-  <v-sheet class="mx-auto pa-4 rounded-lg border" elevation="4" max-width="500">
-    <PageTitle title="關於傘人家族" class="mb-2" />
-    <p>
-      「傘人家族」是基隆城市指標系統中擔任城市的小小導覽員，將城市色彩和資訊相結合，串聯城市、生活和交通資源，你可以在孝三路商圈、城隍廟、崁仔頂、委託行商圈和許梓桑古厝等地找到他們喔。<br />
-      目前他們已分佈在 18
-      處，不管你看到的是優雅的「大傘」，俏皮的「小傘」，還是可愛的「小雲」，只要看到傘橘色的傘人家族，就趕緊拿起手機掃一掃
-      QR Code 吧！<br />
-      小傘人們會帶你連接到基隆旅遊網，獲得景點的相關資訊。讓你的基隆旅程更加豐富趣味！
-    </p>
+  <v-sheet
+    class="mx-auto pa-md-8 pa-4 rounded-lg border"
+    elevation="4"
+    max-width="1200"
+  >
+    <h1 class="page-title">關於傘人家族</h1>
+    <h3 class="page-subtitle">城市指標更驚艷</h3>
+
+    <div
+      class="d-flex flex-sm-row flex-column align-center mt-4"
+      style="gap: 2rem"
+    >
+      <v-img
+        src="images/umb-family.png"
+        class="w-sm-25 w-75 d-flex text-center"
+      ></v-img>
+      <div
+        class="paragraph d-flex flex-column align-sm-start align-center justify-end"
+      >
+        <h4 class="paragraph-title">關於傘人家族</h4>
+        <p>
+          「傘人家族」是基隆城市指標系統中擔任城市的小小導覽員，將城市色彩和資訊相結合，串聯城市、生活和交通資源，你可以在孝三路商圈、城隍廟、崁仔頂、委託行商圈和許梓桑古厝等地找到他們喔。目前他們已分佈在
+          18
+          處，不管你看到的是優雅的「大傘」，俏皮的「小傘」，還是可愛的「小雲」，
+          只要看到傘橘色的傘人家族，就趕緊拿起手機掃一掃 QR Code
+          吧！小傘人們會帶你連接到基隆旅遊網，獲得景點的相關資訊。讓你的基隆旅程更加豐富趣味！
+        </p>
+      </div>
+    </div>
+
+    <div class="paragraph d-flex flex-column align-center my-8">
+      <h4 class="paragraph-title">傘人家族大點名</h4>
+      <div class="img-info-container w-100 d-sm-flex" style="gap: 2rem">
+        <div :class="imgInfoClass">
+          <div class="img-container">
+            <v-img src="images/toy-1.png" contain></v-img>
+          </div>
+          <div :class="textInfoClass">
+            <h4 class="orange-bold-text">大傘</h4>
+            <p class="text-sm-center">
+              撐著長傘，以優雅的姿態瀏覽基隆雨都的各種風貌。
+            </p>
+          </div>
+        </div>
+        <div :class="imgInfoClass">
+          <div class="img-container">
+            <v-img src="images/toy-2.png" contain></v-img>
+          </div>
+          <div :class="textInfoClass">
+            <h4 class="orange-bold-text">小傘</h4>
+            <p class="text-sm-center">頂著小傘，活潑且愉快的穿梭在大街小巷。</p>
+          </div>
+        </div>
+        <div :class="imgInfoClass">
+          <div class="img-container">
+            <v-img src="images/toy-3.png" contain></v-img>
+          </div>
+          <div :class="textInfoClass">
+            <h4 class="orange-bold-text">小雲</h4>
+            <p class="text-sm-center">
+              雲朵帶的雨滴，加入成為基隆傘人家族的一員。
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="paragraph d-flex flex-column align-center">
+      <h4 class="paragraph-title">尋找傘人家族</h4>
+      <v-img src="images/family-map.jpg" class="w-100"></v-img>
+    </div>
   </v-sheet>
 
   <HomeModal />
@@ -17,20 +79,31 @@
 import { ref, watch, onUnmounted } from 'vue';
 import { useRouter } from 'vue-router';
 
-import PageTitle from '@/components/PageTitle.vue';
 import HomeModal from '@/components/HomeModal.vue';
 
 const router = useRouter();
 
 const HOME_TITLE = '就愛和你拍';
 const SUBTITLE = '〈傘人家族〉創意合照競賽';
+
+const imgInfoClass = [
+  'img-info',
+  'd-flex',
+  'flex-sm-column',
+  'align-center',
+  'w-sm-33',
+];
+const textInfoClass = ['text-sm-center', 'w-75', 'w-sm-100'];
 </script>
 
 <style scoped lang="scss">
-@import '@/assets/styles/buttons.scss';
-.page-title {
-  display: flex;
-  align-items: center;
-  letter-spacing: 1.4px;
+.img-container {
+  margin-bottom: 1rem;
+  width: 200px;
+  height: 200px;
+  @media only screen and (max-width: 960px) {
+    width: 100px;
+    height: 100px;
+  }
 }
 </style>
