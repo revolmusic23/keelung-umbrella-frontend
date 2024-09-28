e1dec092d342f6c50b681be2787ee07ce27523fa
revolmusic23 <revolmusic23@gmail.com>
2024-09-28 21:14:04 +0800

    fix: 把活動頁面 h3 外面的 p 標籤拿掉

--- src/views/Activity.vue
+++ src/views/Activity.vue
@@ -4,24 +4,11 @@
     elevation="4"
     max-width="1200"
   >
-
-  <header class="banner">
-      <v-img
-        src="images/cloud.svg"
-        class="cloud-1"
-      ></v-img>
-      <v-img
-        src="images/cloud.svg"
-        class="cloud-2"
-      ></v-img>
-      <v-img
-        src="images/cloud.svg"
-        class="cloud-3"
-      ></v-img>
-      <v-img
-        src="images/sun.svg"
-        class="sun"
-      ></v-img>
+    <header class="banner">
+      <v-img src="images/cloud.svg" class="cloud-1"></v-img>
+      <v-img src="images/cloud.svg" class="cloud-2"></v-img>
+      <v-img src="images/cloud.svg" class="cloud-3"></v-img>
+      <v-img src="images/sun.svg" class="sun"></v-img>
       <div class="banner-container">
         <div
           class="d-flex flex-sm-row flex-column align-center"
@@ -35,208 +22,223 @@
             class="d-flex flex-column align-sm-start align-center justify-end"
           >
             <h1 class="banner-container-title">一起「傘人」1~2~3!</h1>
-            <h3 class="banner-container-subtitle">傘人家族創意合照 - <br class="break">手機攝影比賽</h3>
+            <h3 class="banner-container-subtitle">
+              傘人家族創意合照 - <br class="break" />手機攝影比賽
+            </h3>
           </div>
         </div>
       </div>
     </header>
 
-  <article class="article">
-    <div class="article-container">
-      <div class="article-container-title">
-        <h4 class="article-container-title-text">活動內容</h4>
-      </div>
-      
-      <p class="article-container-content">
-        10/10～10/31 活動期間至基隆市文化廣場與任一傘人家族成員合照，合照對象不限(人、竉物、玩偶、玩具…皆可)，再至活動網站，選擇一張合照並留下一句話上傳作品，即完成參賽，活動結束後將選出前三名最有創意的照片，贈予精美獎品。
-      </p>
-    </div>
-  </article>
+    <article class="article">
+      <div class="article-container">
+        <div class="article-container-title">
+          <h4 class="article-container-title-text">活動內容</h4>
+        </div>
 
-  <main class="information">
-    <div class="information-container">
-      <div class="d-lg-flex justify-space-between align-end gx-6">
-        <p>
+        <p class="article-container-content">
+          10/10～10/31
+          活動期間至基隆市文化廣場與任一傘人家族成員合照，合照對象不限(人、竉物、玩偶、玩具…皆可)，再至活動網站，選擇一張合照並留下一句話上傳作品，即完成參賽，活動結束後將選出前三名最有創意的照片，贈予精美獎品。
+        </p>
+      </div>
+    </article>
+
+    <main class="information">
+      <div class="information-container">
+        <div class="d-lg-flex justify-space-between align-end gx-6">
           <h3 class="paragraph-title">活動獎項</h3>
 
           <div class="information-container-prize">
-            <h4
-            >第 1 名：LEICA 徠卡 SOFORT 2 拍立得相機</h4
+            <h4>第 1 名：LEICA 徠卡 SOFORT 2 拍立得相機</h4>
+            <h4>
+              第 2 名：Polaroid 寶麗來 Now+ G2 拍立得相機套組
+              <span>( 含 5 種顏色濾鏡 + 底片 )</span>
+            </h4>
+            <h4>第 3 名：FUJIFILM 富士 instax SQUARE SQ1 拍立得相機</h4>
+          </div>
+          <div
+            class="img-container d-flex w-lg-50 w-md-75 w-sm-75 w-100 mt-lg-0 mt-8"
+            style="gap: 1.2rem"
           >
-          <h4>第 2 名：Polaroid 寶麗來 Now+ G2 拍立得相機套組 <span>( 含 5 種顏色濾鏡 + 底片 )</span></h4
+            <v-img src="images/prize-1.png">
+              <div class="prize-medal">1</div>
+            </v-img>
+            <v-img src="images/prize-2.png">
+              <div class="prize-medal">2</div>
+            </v-img>
+            <v-img src="images/prize-3.png">
+              <div class="prize-medal">3</div>
+            </v-img>
+          </div>
+        </div>
+
+        <div class="information-container-gift">
+          <div class="information-container-gift-left">
+            <p>活動<br />好禮</p>
+          </div>
+          <div class="information-container-gift-right">
+            活動期間每日
+            <span>9:00 - 17:00 於活動現場</span>完成參賽，分享活動網站於個人IG或
+            FB 並追蹤基隆市文化觀光局 FB
+            ，經現場工作人員確認無誤後，將可獲得活動特製<span>「傘人家族精緻小物」</span>一個（二款隨機贈送，一人限領取一個，每日限量，送完為止）。<span
+              >10/10、10/13、10/14、10/21、10/28</span
+            >
+            為休館日，無兌換活動，但仍可拍照參賽。
+          </div>
+        </div>
+
+        <div class="d-md-flex justify-start align-center mt-12 gx-6">
+          <div
+            class="img-container w-25 mt-lg-0 mt-8 mr-6 d-none d-md-flex"
+            style="gap: 2rem"
           >
-          <h4>第 3 名：FUJIFILM 富士 instax SQUARE SQ1 拍立得相機</h4
-          >
+            <v-img src="images/photo-1.png"></v-img>
           </div>
-        </p>
-        <div
-          class="img-container d-flex w-lg-50 w-md-75 w-sm-75 w-100 mt-lg-0 mt-8"
-          style="gap: 1.2rem"
-        >
-          <v-img src="images/prize-1.png">
-            <div class="prize-medal">1</div>
-          </v-img>
-          <v-img src="images/prize-2.png">
-            <div class="prize-medal">2</div>
-          </v-img>
-          <v-img src="images/prize-3.png">
-            <div class="prize-medal">3</div>
-          </v-img>
-        </div>
-      </div>
 
-      <div class="information-container-gift">
-        <div class="information-container-gift-left">
-          <p>活動<br>好禮</p>
-        </div>
-        <div class="information-container-gift-right">
-          活動期間每日 <span>9:00 - 17:00 於活動現場</span>完成參賽，分享活動網站於個人IG或 FB 並追蹤基隆市文化觀光局 FB ，經現場工作人員確認無誤後，將可獲得活動特製<span>「傘人家族精緻小物」</span>一個（二款隨機贈送，一人限領取一個，每日限量，送完為止）。<span>10/10、10/13、10/14、10/21、10/28</span> 為休館日，無兌換活動，但仍可拍照參賽。
-        </div>
-      </div>
-
-      <div class="d-md-flex justify-start align-center mt-12 gx-6">
-        <div
-          class="img-container  w-25 mt-lg-0 mt-8 mr-6 d-none d-md-flex"
-          style="gap: 2rem"
-        >
-          <v-img src="images/photo-1.png"></v-img>
-        </div>
-          
-        <div class="information-container-content w-md-75 w-100">
-          <h3 class="paragraph-title">參賽辦法</h3>
-          <ol class="pl-4">
-            <li>作品限用手機拍攝，不限手機品牌。</li>
-            <li>主題內容不限，唯必須與傘人家族任一成員合照，合照對象不限 (人、竉物、玩偶、玩具…皆可)，但照片內容不可違反公序良俗，如有違背，主辦單位有權下架作品，不另行通知。</li>
-            <li>參賽作品需在基隆市文化廣場傘人攝影活動場地拍攝之照片，且同意授權主辦單位於平台宣傳、展示使用權利。</li>
-            <li>每人至多可上傳三張照片參賽，每張照片檔案不可小於 1,200 萬畫素，⻑寬⽐為 4:3 的 JPEG 檔。</li>
-            <li>作品後製僅限用手機調整明暗對⽐、⾊彩、裁切；禁止數位合成、加字、彩繪、疊片等。</li>
-            <li>每張照⽚須附上 50 字以內之敘述，文字內容禁止使用低俗、淫穢、粗鄙等不雅文字，如違反規定，主辦單位有權下架作品，不另行通知。</li>
-          </ol>
-        </div>
-      </div>
-    </div>
-  </main>
-
-  <section class="detail">
-    <div class="detail-container">
-      <div class="d-sm-flex justify-space-between">
-        <div class="w-sm-33 w-100 mr-sm-10 mr-0">
-          <div class="detail-container-section">
-            <h4 class="detail-container-section-title">收件時間</h4>
-            <p class="detail-container-section-content">
-              2024 年
-              <span class="date-text">10/10</span> 至
-              <span class="date-text">10/31</span>
-              止，逾期者恕不受理。
-            </p>
-          </div>
-          <div class="detail-container-section">
-            <h4 class="detail-container-section-title">收件方式</h4>
-            <p class="detail-container-section-content">
-              一律於活動網站，上傳作品
-            </p>
-          </div>
-        </div>
-        <div class="w-sm-66 w-100">
-          <div class="detail-container-section">
-            <h4 class="detail-container-section-title">評分方式</h4>
-            <p class="detail-container-section-content">
-              聘邀影像藝術家、設計總監朱善修博士及網紅伊米塔擔任活動評審
-            </p>
-            <p class="detail-container-section-content mt-6">
-              評分項目
-            </p>
-            <v-table class="border">
-              <thead>
-                <tr>
-                  <th class="text-center">創意</th>
-                  <th class="text-center">文案</th>
-                  <th class="text-center">品質</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>60%</td>
-                  <td>30%</td>
-                  <td>20%</td>
-                </tr>
-              </tbody>
-            </v-table>
+          <div class="information-container-content w-md-75 w-100">
+            <h3 class="paragraph-title">參賽辦法</h3>
+            <ol class="pl-4">
+              <li>作品限用手機拍攝，不限手機品牌。</li>
+              <li>
+                主題內容不限，唯必須與傘人家族任一成員合照，合照對象不限
+                (人、竉物、玩偶、玩具…皆可)，但照片內容不可違反公序良俗，如有違背，主辦單位有權下架作品，不另行通知。
+              </li>
+              <li>
+                參賽作品需在基隆市文化廣場傘人攝影活動場地拍攝之照片，且同意授權主辦單位於平台宣傳、展示使用權利。
+              </li>
+              <li>
+                每人至多可上傳三張照片參賽，每張照片檔案不可小於 1,200
+                萬畫素，⻑寬⽐為 4:3 的 JPEG 檔。
+              </li>
+              <li>
+                作品後製僅限用手機調整明暗對⽐、⾊彩、裁切；禁止數位合成、加字、彩繪、疊片等。
+              </li>
+              <li>
+                每張照⽚須附上 50
+                字以內之敘述，文字內容禁止使用低俗、淫穢、粗鄙等不雅文字，如違反規定，主辦單位有權下架作品，不另行通知。
+              </li>
+            </ol>
           </div>
         </div>
       </div>
-      <div class="detail-container-section">
-        <h4 class="detail-container-section-title">得獎公佈</h4>
-        <p class="detail-container-section-content">
-          <span class="date-text">11/7</span> 將於<span
-            >活動網站、文化觀光局臉書、魅力基隆臉書</span
-          >公佈前三名得獎名單，並通知得獎者領獎。
-        </p>
-      </div>
+    </main>
 
-      <div class="detail-container-section mb-0">
-      <h4 class="detail-container-section-title">注意事項</h4>
-      <div class="detail-container-section-content">
-        <ol class="pl-4">
-          <li>
-            基隆市政府 員工、評審及配偶及二親等以內血 姻 親 均 不得參與比賽 ，有違
-            反而得獎者，撤銷其參賽與得獎資格。
-          </li>
-          <li>
-            凡上傳作品完成報名參加本活動者，即視為已充分瞭解本辦法中各項條款，
-            且願意完全遵守本活動之各項規定。
-          </li>
-          <li>
-            參賽作品不得含有任何具挑釁、猥褻、毀謗、暴力、情色、種族歧視性或其
-            他任何引人厭惡或不合宜之內容，主辦單位得自行判斷並 有權下架作品不另
-            行通知 。
-          </li>
-          <li>
-            主辦單位保留隨時修正、暫停、終止或解釋本活動之一切及 最 終權利（包括
-            但不限於更換活動、提前終止或延長活動時間之 最 終決定權等事項），相關
-            內容如有變動將於 比賽 活動 網 站 及 文化觀光局臉書
-            另行公告，不另行通知，
-            若有任何異動，以本競賽活動官方網站最新公告為準。
-          </li>
-          <li>
-            若得獎獎項價值累計超過新台幣 1,001 元 含 以上者而未滿 1 8 歲之中獎
-            者，視同父母或監護人中獎，由父母或監護人代為領獎，並請附上得獎者及
-            其代領人關係證明影本。
-          </li>
-          <li>
-            本活動如有任何因電腦、網路、技術或其他不可歸責於主辦單位之事由，而
-            使活動參加者所登錄之資料有所遺失、錯誤、無法辨識或毀損所導致資料無
-            效之狀況，主辦單位不負任何法律責任，本活動參加者及獲採用者亦不得異
-            議。如發生前述情形，主辦單位將不做任何通知。
-          </li>
-          <li>
-            本活動參加者應聲明及保證其所填寫或提出之資料均為真實，且未冒用或盜
-            用任何第三人之資料，如冒用或盜用任何第三人資料之法律責任由該參加者
-            自行負擔，概與主辦單位無關，參賽者保證所有填寫或提出之資料均為真實
-            自行負擔，概與主辦單位無關，參賽者保證所有填寫或提出之資料均為真實且正確，且未冒用或盜用任何第三人之資料。如有不實或不正確之情事，將且正確，且未冒用或盜用任何第三人之資料。如有不實或不正確之情事，將被被取消參加或得獎資格。如因此導致主辦單位無法通知其得獎訊息時，主辦取消參加或得獎資格。如因此導致主辦單位無法通知其得獎訊息時，主辦單位不負任何責任，且如有致損害於主辦單位或其他任何第三人，參賽者應單位不負任何責任，且如有致損害於主辦單位或其他任何第三人，參賽者應自負一切相關責任。自負一切相關責任。
-          </li>
-          <li>
-            參賽者在參賽之同時，即代表參賽者同意且未違反與第三者之協議、保證參參賽者在參賽之同時，即代表參賽者同意且未違反與第三者之協議、保證參賽作品未侵犯其他任何個人或實體之版權、商標權、人格權、隱私／公開賽作品未侵犯其他任何個人或實體之版權、商標權、人格權、隱私／公開權、以及智慧財產權，且未有第三者對該參賽作品具備任何權利、所有權、權、以及智慧財產權，且未有第三者對該參賽作品具備任何權利、所有權、主張擁有或相關利益，亦未有違反其他法令情事。如有違反之虞，主辦單位主張擁有或相關利益，亦未有違反其他法令情事。如有違反之虞，主辦單位得立即移除相關內容，並取消其參賽與得獎資格，並追回一切獎項，且參賽得立即移除相關內容，並取消其參賽與得獎資格，並追回一切獎項，且參賽者須自負法律責任。者須自負法律責任。
-          </li>
-          <li>
-            參賽者應尊重評審觀點，對於評審結果不得異議，經評審得獎確定之作品，參賽者應尊重評審觀點，對於評審結果不得異議，經評審得獎確定之作品，不得要求取消得獎資格。不得要求取消得獎資格。
-          </li>
-          <li>
-            若主辦單位因使用參賽者所提供之圖文、照片及其他資料遭第三人請求、索若主辦單位因使用參賽者所提供之圖文、照片及其他資料遭第三人請求、索賠、提出民、刑事訴訟或主張任何法律上之權利，參賽者應賠償主辦單位因賠、提出民、刑事訴訟或主張任何法律上之權利，參賽者應賠償主辦單位因此所生之全部損失與費用。此所生之全部損失與費用。
-          </li>
-          <li>
-            參賽者如因參加本活動或因活動獎項而遭受任何損失，主辦單位不負任何責參賽者如因參加本活動或因活動獎項而遭受任何損失，主辦單位不負任何責任。一旦得獎者領取獎品後，若有遺失或被竊，主辦單位不發給任何證明或任。一旦得獎者領取獎品後，若有遺失或被竊，主辦單位不發給任何證明或補償。補償。
-          </li>
-          <li>
-            本辦法若有其他未盡事宜，悉依主辦單位相關規定或解釋辦理，主辦單位保本辦法若有其他未盡事宜，悉依主辦單位相關規定或解釋辦理，主辦單位保有有最最終之解釋權。終之解釋權。
-          </li>
-        </ol>
-      </div>
-    </div>
-    </div>
-  </section>
+    <section class="detail">
+      <div class="detail-container">
+        <div class="d-sm-flex justify-space-between">
+          <div class="w-sm-33 w-100 mr-sm-10 mr-0">
+            <div class="detail-container-section">
+              <h4 class="detail-container-section-title">收件時間</h4>
+              <p class="detail-container-section-content">
+                2024 年
+                <span class="date-text">10/10</span> 至
+                <span class="date-text">10/31</span>
+                止，逾期者恕不受理。
+              </p>
+            </div>
+            <div class="detail-container-section">
+              <h4 class="detail-container-section-title">收件方式</h4>
+              <p class="detail-container-section-content">
+                一律於活動網站，上傳作品
+              </p>
+            </div>
+          </div>
+          <div class="w-sm-66 w-100">
+            <div class="detail-container-section">
+              <h4 class="detail-container-section-title">評分方式</h4>
+              <p class="detail-container-section-content">
+                聘邀影像藝術家、設計總監朱善修博士及網紅伊米塔擔任活動評審
+              </p>
+              <p class="detail-container-section-content mt-6">評分項目</p>
+              <v-table class="border">
+                <thead>
+                  <tr>
+                    <th class="text-center">創意</th>
+                    <th class="text-center">文案</th>
+                    <th class="text-center">品質</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>60%</td>
+                    <td>30%</td>
+                    <td>20%</td>
+                  </tr>
+                </tbody>
+              </v-table>
+            </div>
+          </div>
+        </div>
+        <div class="detail-container-section">
+          <h4 class="detail-container-section-title">得獎公佈</h4>
+          <p class="detail-container-section-content">
+            <span class="date-text">11/7</span>
+            將於<span>活動網站、文化觀光局臉書、魅力基隆臉書</span>公佈前三名得獎名單，並通知得獎者領獎。
+          </p>
+        </div>
 
-    
+        <div class="detail-container-section mb-0">
+          <h4 class="detail-container-section-title">注意事項</h4>
+          <div class="detail-container-section-content">
+            <ol class="pl-4">
+              <li>
+                基隆市政府 員工、評審及配偶及二親等以內血 姻 親 均 不得參與比賽
+                ，有違 反而得獎者，撤銷其參賽與得獎資格。
+              </li>
+              <li>
+                凡上傳作品完成報名參加本活動者，即視為已充分瞭解本辦法中各項條款，
+                且願意完全遵守本活動之各項規定。
+              </li>
+              <li>
+                參賽作品不得含有任何具挑釁、猥褻、毀謗、暴力、情色、種族歧視性或其
+                他任何引人厭惡或不合宜之內容，主辦單位得自行判斷並
+                有權下架作品不另 行通知 。
+              </li>
+              <li>
+                主辦單位保留隨時修正、暫停、終止或解釋本活動之一切及 最
+                終權利（包括 但不限於更換活動、提前終止或延長活動時間之 最
+                終決定權等事項），相關 內容如有變動將於 比賽 活動 網 站 及
+                文化觀光局臉書 另行公告，不另行通知，
+                若有任何異動，以本競賽活動官方網站最新公告為準。
+              </li>
+              <li>
+                若得獎獎項價值累計超過新台幣 1,001 元 含 以上者而未滿 1 8
+                歲之中獎
+                者，視同父母或監護人中獎，由父母或監護人代為領獎，並請附上得獎者及
+                其代領人關係證明影本。
+              </li>
+              <li>
+                本活動如有任何因電腦、網路、技術或其他不可歸責於主辦單位之事由，而
+                使活動參加者所登錄之資料有所遺失、錯誤、無法辨識或毀損所導致資料無
+                效之狀況，主辦單位不負任何法律責任，本活動參加者及獲採用者亦不得異
+                議。如發生前述情形，主辦單位將不做任何通知。
+              </li>
+              <li>
+                本活動參加者應聲明及保證其所填寫或提出之資料均為真實，且未冒用或盜
+                用任何第三人之資料，如冒用或盜用任何第三人資料之法律責任由該參加者
+                自行負擔，概與主辦單位無關，參賽者保證所有填寫或提出之資料均為真實
+                自行負擔，概與主辦單位無關，參賽者保證所有填寫或提出之資料均為真實且正確，且未冒用或盜用任何第三人之資料。如有不實或不正確之情事，將且正確，且未冒用或盜用任何第三人之資料。如有不實或不正確之情事，將被被取消參加或得獎資格。如因此導致主辦單位無法通知其得獎訊息時，主辦取消參加或得獎資格。如因此導致主辦單位無法通知其得獎訊息時，主辦單位不負任何責任，且如有致損害於主辦單位或其他任何第三人，參賽者應單位不負任何責任，且如有致損害於主辦單位或其他任何第三人，參賽者應自負一切相關責任。自負一切相關責任。
+              </li>
+              <li>
+                參賽者在參賽之同時，即代表參賽者同意且未違反與第三者之協議、保證參參賽者在參賽之同時，即代表參賽者同意且未違反與第三者之協議、保證參賽作品未侵犯其他任何個人或實體之版權、商標權、人格權、隱私／公開賽作品未侵犯其他任何個人或實體之版權、商標權、人格權、隱私／公開權、以及智慧財產權，且未有第三者對該參賽作品具備任何權利、所有權、權、以及智慧財產權，且未有第三者對該參賽作品具備任何權利、所有權、主張擁有或相關利益，亦未有違反其他法令情事。如有違反之虞，主辦單位主張擁有或相關利益，亦未有違反其他法令情事。如有違反之虞，主辦單位得立即移除相關內容，並取消其參賽與得獎資格，並追回一切獎項，且參賽得立即移除相關內容，並取消其參賽與得獎資格，並追回一切獎項，且參賽者須自負法律責任。者須自負法律責任。
+              </li>
+              <li>
+                參賽者應尊重評審觀點，對於評審結果不得異議，經評審得獎確定之作品，參賽者應尊重評審觀點，對於評審結果不得異議，經評審得獎確定之作品，不得要求取消得獎資格。不得要求取消得獎資格。
+              </li>
+              <li>
+                若主辦單位因使用參賽者所提供之圖文、照片及其他資料遭第三人請求、索若主辦單位因使用參賽者所提供之圖文、照片及其他資料遭第三人請求、索賠、提出民、刑事訴訟或主張任何法律上之權利，參賽者應賠償主辦單位因賠、提出民、刑事訴訟或主張任何法律上之權利，參賽者應賠償主辦單位因此所生之全部損失與費用。此所生之全部損失與費用。
+              </li>
+              <li>
+                參賽者如因參加本活動或因活動獎項而遭受任何損失，主辦單位不負任何責參賽者如因參加本活動或因活動獎項而遭受任何損失，主辦單位不負任何責任。一旦得獎者領取獎品後，若有遺失或被竊，主辦單位不發給任何證明或任。一旦得獎者領取獎品後，若有遺失或被竊，主辦單位不發給任何證明或補償。補償。
+              </li>
+              <li>
+                本辦法若有其他未盡事宜，悉依主辦單位相關規定或解釋辦理，主辦單位保本辦法若有其他未盡事宜，悉依主辦單位相關規定或解釋辦理，主辦單位保有有最最終之解釋權。終之解釋權。
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </section>
   </v-sheet>
 </template>
 
@@ -249,7 +251,6 @@ import PageTitle from '@/components/PageTitle.vue';
 </script>
 
 <style lang="scss" scoped>
-
 .banner {
   background-color: var(--primary-water);
   position: relative;
@@ -309,8 +310,8 @@ import PageTitle from '@/components/PageTitle.vue';
       font-weight: 700;
 
       @media only screen and (min-width: 601px) and (max-width: 1280px) {
-          font-size: 2.4vw;
-        }
+        font-size: 2.4vw;
+      }
 
       @media only screen and (max-width: 600px) {
         font-size: 20px;
