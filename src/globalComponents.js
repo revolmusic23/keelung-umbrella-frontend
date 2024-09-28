8b98b2e1b613467c05816b42dbd960c35c50b877
revolmusic23 <revolmusic23@gmail.com>
2024-09-28 13:16:45 +0800

    feat: 攔截器發生錯誤會顯示 error modal

--- src/globalComponents.js
+++ src/globalComponents.js
@@ -1,9 +1,11 @@
 import LoadingCircle from './components/LoadingCircle.vue';
 import CardImage from './components/CardImage.vue';
+import ErrorModal from './components/Modal/ErrorModal.vue';
 
 export default {
   install: (app) => {
     app.component('XLoadingCircle', LoadingCircle);
     app.component('XCardImage', CardImage);
+    app.component('XErrorModal', ErrorModal);
   },
 };
